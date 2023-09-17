6690a64157e79233980895324f67c480b34587af
hueforge <111766391+reynolkw@users.noreply.github.com>
2023-09-16 23:38:54 -0400

    added functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final double SALES_TAX = 0.05;
+        // simulated user input
+        double purchasePrice = 100.0;
+        double computedSalesTax = 0.0;
+        double totalCost = 0.0;
+
+        computedSalesTax = purchasePrice * SALES_TAX;
+        totalCost = purchasePrice + computedSalesTax;
+
+        System.out.println("The sales tax for $" + purchasePrice + " is $" + computedSalesTax + ".");
+        System.out.println("The total of the transaction is $" + totalCost);
     }
 }
